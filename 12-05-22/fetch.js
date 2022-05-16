4ee67f620e0603be97ffec4c4692fdd580d0f670
Pruthvi-sagar <pruthvi.sagar@appinessworld.com>
2022-05-16 09:02:04 +0530

    changes

---
+++ 12-05-22/fetch.js
@@ -0,0 +1,26 @@
+// fetch("https://swapi.dev/api/people/1/")
+// .then ((res)=> {
+//     console.log("RESOLVED!", res);
+//    return res.json();
+
+// })
+
+// .then((data) => {
+//     console.log(data);
+//    return fetch("https://swapi.dev/api/people/2/")
+// })
+// .then(res => {
+//     console.log("second request resolved");
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log(data);
+   
+// })
+// .catch((e) => {
+//     console.log("ERROR", e);
+// })
+
+ 
+
+const loadStarWarsPeople
